1803f2cfabd53e30ecb09618ffa8ab95e824671c
Tolulomo <tolulomofateye@gmail.com>
2021-03-05 11:41:25 -0500

    install updates

--- install/install.sh
+++ install/install.sh
@@ -29,6 +29,7 @@ if [[ -z $(which pip) ]]; then
   echo 'Installing pip - python package installer'
   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
   (su root -c 'python get-pip.py')
+  (su root -c 'pip install --upgrade pip')
 else
   echo 'Already installed: pip -- updating pip'
   (su root -c 'pip install --upgrade pip')
