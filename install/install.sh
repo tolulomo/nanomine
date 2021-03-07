0127891b501459027bf003cf222daceaeb209ae0
Tolulomo <tolulomofateye@gmail.com>
2021-03-07 17:05:17 -0500

    test

--- install/install.sh
+++ install/install.sh
@@ -26,51 +26,51 @@ if [[ $(whoami) != "root" ]]; then
 fi
 
 echo installing python3.7
-# Install requirements
-apt-get install -y build-essential \
-checkinstall \
-libreadline-gplv2-dev \
-libncursesw5-dev \
-libssl-dev \
-libsqlite3-dev \
-tk-dev \
-libgdbm-dev \
-libc6-dev \
-libbz2-dev \
-zlib1g-dev \
-openssl \
-libffi-dev \
-python3-dev \
-python3-setuptools \
-wget
+# # Install requirements
+# apt-get install -y build-essential \
+# checkinstall \
+# libreadline-gplv2-dev \
+# libncursesw5-dev \
+# libssl-dev \
+# libsqlite3-dev \
+# tk-dev \
+# libgdbm-dev \
+# libc6-dev \
+# libbz2-dev \
+# zlib1g-dev \
+# openssl \
+# libffi-dev \
+# python3-dev \
+# python3-setuptools \
+# wget
 
-# Prepare to build
-mkdir /tmp/Python37
-cd /tmp/Python37
+# # Prepare to build
+# mkdir /tmp/Python37
+# cd /tmp/Python37
 
-# Pull down Python 3.7, build, and install
-wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz
-tar xvf Python-3.7.0.tar.xz
-cd /tmp/Python37/Python-3.7.0
-./configure
-make altinstall
+# # Pull down Python 3.7, build, and install
+# wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz
+# tar xvf Python-3.7.0.tar.xz
+# cd /tmp/Python37/Python-3.7.0
+# ./configure
+# make altinstall
 
-echo 'alias python=python3.7' >> ~/.bashrc
-echo 'alias pip=pip3.7' >> ~/.bashrc
-source ~/.bashrc
+# echo 'alias python=python3.7' >> ~/.bashrc
+# echo 'alias pip=pip3.7' >> ~/.bashrc
+# source ~/.bashrc
 
-echo Updating pip
-pip install --upgrade pip
+# echo Updating pip
+# pip install --upgrade pip
 
 
-# if [[ -z $(which pip) ]]; then
-#   echo 'Installing pip - python package installer'
-#   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-#   (su root -c 'python3 get-pip.py')
-# else
-#   echo 'Already installed: pip -- updating pip'
-#   (su root -c 'pip3 install --upgrade pip')
-# fi
+if [[ -z $(which pip) ]]; then
+  echo 'Installing pip - python package installer'
+  curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+  (su root -c 'python3 get-pip.py')
+else
+  echo 'Already installed: pip -- updating pip'
+  (su root -c 'pip3 install --upgrade pip')
+fi
 
 echo installing git
 apt-get install -y git
